5ef9ff145ef0676d4d8f19446d4c397dd81d326e
racpsjcsp@gmail.com <racpsjcsp@gmail.com>
2021-02-28 18:26:27 -0300

    config images to display if miss/hit/reset game

--- hangman/GameVC.swift
+++ hangman/GameVC.swift
@@ -10,39 +10,31 @@ import UIKit
 class GameVC: UIViewController {
 
     var username: String!
-    let wordList = ["Abnegation", "Aggrandize", "Alacrity", "Beguile", "Blandishment", "Callous", "Camaraderie", "Cognizant",                     "Convivial", "Construe", "Demagogue", "Denigrate", "Didactic", "Disparate", "Egregious", "Embezzlement",
-                    "Enervate", "Ephemeral", "Equanimity","Fatuous", "Gratuitous", "Iconoclast",
-                    "Incumbent", "Inveterate","Idiosyncratic", "Quotidian", "Promulgate", "Sanctimonious",
-                    "Recalcitrant", "Vociferous", "Vicissitude", "Solipsism", "Pertinacious", "Obdurate", "Licentious",
-                    "Mendacious"]
+    let wordList = ["Abnegation", "Aggrandize", "Alacrity", "Beguile", "Blandishment", "Callous", "Camaraderie",
+                    "Cognizant", "Convivial", "Construe", "Demagogue", "Denigrate", "Didactic", "Disparate",
+                    "Egregious", "Embezzlement", "Enervate", "Ephemeral", "Equanimity","Fatuous", "Gratuitous",
+                    "Iconoclast", "Incumbent", "Inveterate","Idiosyncratic", "Quotidian", "Promulgate",
+                    "Sanctimonious", "Recalcitrant", "Vociferous", "Vicissitude", "Solipsism", "Pertinacious",
+                    "Obdurate", "Licentious", "Mendacious"]
     
     var hiddenWord: [Character] = []
     var pickedWord: [Character] = []
     var numberOfGuesses: Int = 6
     
     var timer = Timer()
-    
-    private lazy var hangmanImage = UIImageView()
+
     private lazy var hideContainer = UIView()
-    private lazy var hideBlock = CAShapeLayer()
     
-    
+    private lazy var hangmanImage = UIImage(named: "hangman")
     private lazy var hangmanContainer = UIImageView()
-    
-    private lazy var leftArm = UIImageView()
-    private lazy var rightArm = UIImageView()
-    private lazy var leftLeg = UIImageView()
-    private lazy var rightLeg = UIImageView()
-    private lazy var head = UIImageView()
-    private lazy var torso = UIImageView()
-    private lazy var rightArmShoulder = UIImageView()
-    private lazy var cordPiece = UIImageView()
-    
-    private lazy var upperPole = UIImageView()
-    private lazy var basePole = UIImageView()
-    
-    
-    
+    private lazy var hideHead = UIView()
+    private lazy var hideLeftArm = UIView()
+    private lazy var hideTorso = UIView()
+    private lazy var hideRightArm = UIView()
+    private lazy var hideLeftLeg = UIView()
+    private lazy var hideRightLeg = UIView()
+    private lazy var bodyParts = [hideHead, hideLeftArm, hideTorso, hideRightArm, hideRightLeg, hideLeftLeg]
+
     private lazy var alertContainer = HMAlertContainerView()
     private lazy var wordLabel = HMLabel()
     
@@ -84,7 +76,7 @@ class GameVC: UIViewController {
          buttonO, buttonP, buttonQ, buttonR, buttonS, buttonT, buttonU,
          buttonV, buttonW, buttonX, buttonY, buttonZ]
     }
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -92,14 +84,12 @@ class GameVC: UIViewController {
         title = "GAME"
         navigationController?.isNavigationBarHidden = false
         
-        setupHangmanContainer()
-        setupUpperPole()
-//        setupHideBlocks()
         setupFourthRowButtonStackView()
         setupThirdRowButtonStackView()
         setupSecondRowButtonStackView()
         setupFirstRowButtonStackView()
         setupWordLabel()
+        setupHangmanContainer()
         pickupRandomWord()
         detectButtonPressed()
         
@@ -169,6 +159,10 @@ class GameVC: UIViewController {
             button?.isEnabled = true
             button?.backgroundColor = .systemBlue
         }
+
+        for bodyPart in bodyParts {
+            bodyPart.backgroundColor = .systemGray6
+        }
     }
     
     
@@ -223,7 +217,7 @@ class GameVC: UIViewController {
     }
     
     @objc func buttonPressed(_ sender: UIButton) {
-        
+
         if let letterPressed = sender.titleLabel?.text {
             for (i, char) in pickedWord.enumerated() {
                 if char == Character(letterPressed) {
@@ -240,14 +234,31 @@ class GameVC: UIViewController {
                 sender.backgroundColor = .systemRed
                 sender.alpha = 0.9
                 sender.isEnabled = false
-                
+
                 numberOfGuesses -= 1
+
+                if numberOfGuesses == 5 {
+                    hideHead.backgroundColor = .clear
+                } else if numberOfGuesses == 4 {
+                    hideTorso.backgroundColor = .clear
+
+                } else if numberOfGuesses == 3 {
+                    hideLeftArm.backgroundColor = .clear
+
+                } else if numberOfGuesses == 2 {
+                    hideRightArm.backgroundColor = .clear
+
+                } else if numberOfGuesses == 1 {
+                    hideLeftLeg.backgroundColor = .clear
+
+                } else {
+                    hideLeftLeg.backgroundColor = .clear
+
+                }
             }
             
             wordLabel.text = String(hiddenWord)
-            
             checkIfWon()
-
         }
     }
 }
@@ -258,89 +269,80 @@ extension GameVC {
     
     func setupHangmanContainer() {
         view.addSubview(hangmanContainer)
-        
-//        hangmanContainer.addSubview(upperPole)
-//        hangmanContainer.addSubview(basePole)
-//        hangmanContainer.addSubview(cordPiece)
-//        hangmanContainer.addSubview(head)
-//        hangmanContainer.addSubview(torso)
-//        hangmanContainer.addSubview(leftArm)
-//        hangmanContainer.addSubview(rightArm)
-//        hangmanContainer.addSubview(rightArmShoulder)
-//        hangmanContainer.addSubview(leftLeg)
-//        hangmanContainer.addSubview(rightLeg)
+        hangmanContainer.addSubview(hideHead)
+        hangmanContainer.addSubview(hideTorso)
+        hangmanContainer.addSubview(hideLeftArm)
+        hangmanContainer.addSubview(hideRightArm)
+        hangmanContainer.addSubview(hideLeftLeg)
+        hangmanContainer.addSubview(hideRightLeg)
+
+        hangmanContainer.image = hangmanImage
 
         hangmanContainer.translatesAutoresizingMaskIntoConstraints = false
-        
-        upperPole.image = UIImage(named: "upperPole")
-        basePole.image = UIImage(named: "basePole")
-        cordPiece.image = UIImage(named: "cordPiece")
-        head.image = UIImage(named: "head")
-        torso.image = UIImage(named: "torso")
-        leftArm.image = UIImage(named: "leftArm")
-        rightArm.image = UIImage(named: "rightArm")
-        rightArmShoulder.image = UIImage(named: "rightArmShoulder")
-        leftLeg.image = UIImage(named: "leftLeg")
-        rightLeg.image = UIImage(named: "rightLeg")
-        hangmanContainer.backgroundColor = .systemGreen
-        
-        
         NSLayoutConstraint.activate([
-            hangmanContainer.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
-            hangmanContainer.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            hangmanContainer.heightAnchor.constraint(equalToConstant: 280),
-            hangmanContainer.widthAnchor.constraint(equalToConstant: 280)
+            hangmanContainer.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 16),
+            hangmanContainer.heightAnchor.constraint(equalToConstant: 300),
+            hangmanContainer.widthAnchor.constraint(equalToConstant: 300),
+            hangmanContainer.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: 0)
         ])
-        
-    }
-    
-    func setupUpperPole() {
-        view.addSubview(upperPole)
-        
-        upperPole.translatesAutoresizingMaskIntoConstraints = false
-        upperPole.image = UIImage(named: "upperPole")
-       
+
+        hideHead.backgroundColor = .systemGray6
+        hideHead.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            upperPole.topAnchor.constraint(equalTo: hangmanContainer.topAnchor),
-            upperPole.centerXAnchor.constraint(equalTo: hangmanContainer.centerXAnchor)
+            hideHead.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 88),
+            hideHead.heightAnchor.constraint(equalToConstant: 60),
+            hideHead.widthAnchor.constraint(equalToConstant: 80),
+            hideHead.leadingAnchor.constraint(equalTo: hangmanContainer.leadingAnchor, constant: 20)
         ])
-        
-    }
-    
-    func setupBasePole() {
-        view.addSubview(basePole)
-        
-        basePole.translatesAutoresizingMaskIntoConstraints = false
-        basePole.image = UIImage(named: "basePole")
+
+        hideTorso.backgroundColor = .systemGray6
+        hideTorso.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            basePole.topAnchor.constraint(equalTo: upperPole.bottomAnchor),
-            basePole.trailingAnchor.constraint(equalTo: hangmanContainer.trailingAnchor)
+            hideTorso.topAnchor.constraint(equalTo: hideHead.bottomAnchor, constant: 0),
+            hideTorso.heightAnchor.constraint(equalToConstant: 80),
+            hideTorso.widthAnchor.constraint(equalToConstant: 40),
+            hideTorso.leadingAnchor.constraint(equalTo: hangmanContainer.leadingAnchor, constant: 48)
         ])
-        
-    }
-    
-    func setupHideBlocks() {
-        view.addSubview(hideContainer)
-//        hideContainer.layer.addSublayer(hideBlock)
-//        hideContainer.isHidden = false
-        
-//        hideBlock.path = UIBezierPath(ovalIn: CGRect(x: 50, y: 50, width: 100, height: 100)).cgPath
-        
-//        hideBlock.strokeColor = UIColor.systemGray6.cgColor
-//        hideBlock.fillColor = UIColor.systemGray6.cgColor
-//        hideBlock.translatesAutoresizingMaskIntoConstraints = false
-//        hideBlock.backgroundColor = .systemRed
-//        hideBlock.layer
-//        hideBlock.layer.cornerRadius = 50
-        
+
+        hideLeftArm.backgroundColor = .systemGray6
+        hideLeftArm.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            hideContainer.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
-            hideContainer.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            hideContainer.heightAnchor.constraint(equalToConstant: 30),
-            hideContainer.widthAnchor.constraint(equalToConstant: 30)
+            hideLeftArm.topAnchor.constraint(equalTo: hideTorso.topAnchor, constant: 0),
+            hideLeftArm.heightAnchor.constraint(equalToConstant: 75),
+            hideLeftArm.widthAnchor.constraint(equalToConstant: 30),
+            hideLeftArm.leadingAnchor.constraint(equalTo: hideHead.leadingAnchor, constant: 0)
         ])
-        
+
+        hideRightArm.backgroundColor = .systemGray6
+        hideRightArm.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            hideRightArm.topAnchor.constraint(equalTo: hideTorso.topAnchor, constant: 0),
+            hideRightArm.heightAnchor.constraint(equalToConstant: 75),
+            hideRightArm.widthAnchor.constraint(equalToConstant: 30),
+            hideRightArm.leadingAnchor.constraint(equalTo: hideTorso.trailingAnchor, constant: 0)
+        ])
+
+        hideLeftLeg.backgroundColor = .systemGray6
+        hideLeftLeg.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            hideLeftLeg.topAnchor.constraint(equalTo: hideTorso.bottomAnchor, constant: 0),
+            hideLeftLeg.heightAnchor.constraint(equalToConstant: 75),
+            hideLeftLeg.widthAnchor.constraint(equalToConstant: 30),
+            hideLeftLeg.trailingAnchor.constraint(equalTo: hideTorso.centerXAnchor, constant: 0)
+        ])
+
+        hideRightLeg.backgroundColor = .systemGray6
+        hideRightLeg.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            hideRightLeg.topAnchor.constraint(equalTo: hideTorso.bottomAnchor, constant: 0),
+            hideRightLeg.heightAnchor.constraint(equalToConstant: 75),
+            hideRightLeg.widthAnchor.constraint(equalToConstant: 30),
+            hideRightLeg.leadingAnchor.constraint(equalTo: hideTorso.centerXAnchor, constant: 0)
+        ])
     }
+ 
+
+
     
     func setupWordLabel() {
         view.addSubview(wordLabel)
@@ -358,12 +360,11 @@ extension GameVC {
         wordLabel.textColor = .black
         
         NSLayoutConstraint.activate([
-            wordLabel.bottomAnchor.constraint(equalTo: firstButtonArrayStackView.topAnchor, constant: -16),
+            wordLabel.bottomAnchor.constraint(equalTo: firstButtonArrayStackView.topAnchor, constant: -8),
             wordLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
             wordLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16),
         ])
-        
-        
+
     }
     
     func setupFirstRowButtonStackView() {
@@ -377,7 +378,7 @@ extension GameVC {
         firstButtonArrayStackView.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
-            firstButtonArrayStackView.bottomAnchor.constraint(equalTo: secondButtonArrayStackView.topAnchor, constant: -10),
+            firstButtonArrayStackView.bottomAnchor.constraint(equalTo: secondButtonArrayStackView.topAnchor, constant: -4),
             firstButtonArrayStackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
             firstButtonArrayStackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16)
             
@@ -403,7 +404,7 @@ extension GameVC {
         secondButtonArrayStackView.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
-            secondButtonArrayStackView.bottomAnchor.constraint(equalTo: thirdButtonArrayStackView.topAnchor, constant: -10),
+            secondButtonArrayStackView.bottomAnchor.constraint(equalTo: thirdButtonArrayStackView.topAnchor, constant: -4),
             secondButtonArrayStackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
             secondButtonArrayStackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16)
             
@@ -429,10 +430,9 @@ extension GameVC {
         thirdButtonArrayStackView.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
-            thirdButtonArrayStackView.bottomAnchor.constraint(equalTo: fourthButtonArrayStackView.topAnchor, constant: -10),
+            thirdButtonArrayStackView.bottomAnchor.constraint(equalTo: fourthButtonArrayStackView.topAnchor, constant: -4),
             thirdButtonArrayStackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
             thirdButtonArrayStackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16)
-            
         ])
         
         thirdButtonArrayStackView.addArrangedSubview(buttonO)
@@ -442,8 +442,6 @@ extension GameVC {
         thirdButtonArrayStackView.addArrangedSubview(buttonS)
         thirdButtonArrayStackView.addArrangedSubview(buttonT)
         thirdButtonArrayStackView.addArrangedSubview(buttonU)
-
-        
     }
     
     func setupFourthRowButtonStackView() {
@@ -457,7 +455,7 @@ extension GameVC {
         fourthButtonArrayStackView.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
-            fourthButtonArrayStackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -30),
+            fourthButtonArrayStackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -8),
             fourthButtonArrayStackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 64),
             fourthButtonArrayStackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -64)
             
